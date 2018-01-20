472c00e1a98f80d174e59bc88e42c1007e8d9b7c
hadihrumman <hadihabrumman@gmail.com>
2018-01-20 17:46:28 +0200

    Initial commit

---
+++ landing/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from landing import views
+
+
+urlpatterns = [
+    #url(r'^$', views. landing, name='landing'),
+    url(r'^$', views.landing, name='landing'),
+
+]
